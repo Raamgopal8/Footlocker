d1cdcc5a2c4ed52274a9e3c8c13e79b4a6dcfd04
Raamgopal8 <raamgopal2369@outlook.com>
2024-08-30 21:42:49 +0530

    update

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import './Loginsignup.css';
 const LoginSignup = () => {
     return (
-        <div>
+        <div className='loginsignup'>
+            <div className="loginsignup-container">
+                <h1>Sign up</h1>
+                <div className="loginfield">
+                    <input type = "text" placeholder='your Name'/>
+                    <input type = "email" placeholder=' Email'/>
+                    <input type = "password" placeholder='Password'/>
+
+                </div>
+                <button>Continue</button>
+                <p className="login-login">Already have an account?<span>Login Here</span></p>
+            <div className="login-agree">
+                <input type="checkbox" name ='' id = '' />
+                <p>BY Continuing ,i agree to terms and conditions.</p>
+            </div>
+            </div>
 
         </div>
     )
---
+++ src/components/Items/Items.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Items.css'
+
+
+const Items = () => {
+   return (
+    <div className="items">
+            
+    </div>
+   )
+}
+export default Items
